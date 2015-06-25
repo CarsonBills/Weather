812788c6e4db40d774e0677779c9f23d3e23a818
CarsonBills <CarsonMBills@Gmail.com>
2015-06-25 13:41:05 -0400

    Forecast for next 5 days. Unstyled

--- limosys.rb
+++ limosys.rb
@@ -4,3 +4,28 @@ Bundler.require
 get "/" do
   erb :root
 end
+
+post "/" do
+  city = params["city"].gsub(" ","+").downcase
+  @current_data = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{city}&APPID=74ff560ad00112e661ced02f92456921")
+  if @current_data["cod"] == "200"
+    erb :result
+  else
+    redirect "error"
+  end
+end
+
+post "/forecast" do
+  city = params["city"]
+  forecast_data = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=#{city}&cnt=5&APPID=74ff560ad00112e661ced02f92456921")
+
+  f = forecast_data["list"].map { |report| "<p>Temp:#{(report["temp"]["day"]- 273.15).round}</p>" }
+
+
+end
+
+get "/error" do
+  erb :error
+end
+
+#http://api.openweathermap.org/data/2.5/forecast/daily?q=new+york&cnt=5&APPID=74ff560ad00112e661ced02f92456921
---
+++ public/scripts.js
@@ -0,0 +1,19 @@
+function clickForecastButton(){
+	$(".forecast_button").on("click", function(e){
+		fetchMenuItem();
+	});
+}
+
+function fetchMenuItem(){
+	var input_city = $("div.city").html().toLowerCase().replace(" ", "+")
+	$.ajax({
+		url : "/forecast",
+		type : "post",
+		data : {city: input_city},
+		success: function(data){
+			var header = "<p> Forecast for the next five days: </p>"
+			$(".forecast").append(header, data);
+		}
+	});
+}
+
